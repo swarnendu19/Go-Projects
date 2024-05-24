36c3df15b19dac50ee82e9cc125c307a5f2123e8
swarnendu19 <swarnendumaity633@gmail.com>
2024-05-24 20:15:32 +0530

    Backend CRUD Movies app

--- go-movies-app/main.go
+++ go-movies-app/main.go
@@ -10,9 +10,9 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// We use struct insted of database
+// We use struct instead of database
 type Movie struct {
-	// ID ,Isbn, title,Director(take from struct Director) make struct of these
+	// ID, Isbn, title,Director(take from struct Director) make struct of these
 	ID       string   `json:"Id"`
 	Isbn     string   `json:"Isbn"`
 	Title    string   `json:"title"`
@@ -20,7 +20,7 @@ type Movie struct {
 }
 
 type Director struct {
-	// firstName , LastName
+	// firstName, LastName
 	FirstName  string `json:"firstName"`
 	SecondName string `json:"secondName"`
 }
@@ -28,7 +28,7 @@ type Director struct {
 var movies []Movie
 
 func main() {
-
+	// Initialize some sample movies
 	movies = append(movies, Movie{
 		ID:    "1",
 		Isbn:  "123456789",
@@ -47,16 +47,19 @@ func main() {
 			SecondName: "Nolan",
 		},
 	})
-	// Declare the routes
-	// Declare 5 routes from the architechture
+
+	// Create a new router
 	r := mux.NewRouter()
+
+	// Define routes
 	r.HandleFunc("/", handleHome).Methods("GET")
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{ID}", getMovie).Methods("GET")
-	r.HandleFunc("/movie", createMovie).Methods("POST")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{ID}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{ID}", deleteMovies).Methods("DELETE")
 
+	// Start the server
 	fmt.Println("Server is connected Successfully on PORT: 8000")
 	err := http.ListenAndServe(":8000", r)
 	if err != nil {
@@ -64,39 +67,42 @@ func main() {
 	}
 }
 
+// handleHome handles requests to the root URL
 func handleHome(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Hello from Home")
 }
 
+// getMovies returns all movies
 func getMovies(w http.ResponseWriter, r *http.Request) {
-	// send the encoded JSOn data from the Database(in this case it is Struct)
+	// send the encoded JSON data from the Database (in this case it is Struct)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(movies)
 }
 
+// deleteMovies deletes a movie with the specified ID
 func deleteMovies(w http.ResponseWriter, r *http.Request) {
-	//set the content type in Json
+	// set the content type to JSON
 	w.Header().Set("Content-Type", "application/json")
-	// store the Id of the movies inside the params
+	// store the ID of the movie inside the params
 	params := mux.Vars(r)
-	//run a for loop in entire movies Slices
-	// If the Id of the movies exist Delete that from Slices
+	// run a for loop over entire movies slice
+	// If the ID of the movie exists, delete it from the slice
 	for index, value := range movies {
-		if params["id"] == value.ID {
+		if params["ID"] == value.ID {
 			movies = append(movies[:index], movies[index+1:]...)
 			return
 		}
 	}
-	// Send the remaining movies in SLices as JSON
+	// Send the remaining movies in the slice as JSON
 	json.NewEncoder(w).Encode(movies)
 }
 
+// getMovie returns a movie with the specified ID
 func getMovie(w http.ResponseWriter, r *http.Request) {
 	// Set the content type to JSON
 	w.Header().Set("Content-Type", "application/json")
 	// Store the ID of the movie from the params
 	params := mux.Vars(r)
-
 	// Iterate over the movies slice
 	// If the ID of the movie matches the requested ID, send the response in JSON format
 	for _, value := range movies {
@@ -113,21 +119,22 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 	http.NotFound(w, r)
 }
 
+// createMovie creates a new movie
 func createMovie(w http.ResponseWriter, r *http.Request) {
-	//set the content type in Json
+	// set the content type to JSON
 	w.Header().Set("Content-Type", "application/json")
-	//Declare new Varaible of type Struct Movie
+	// Declare new variable of type struct Movie
 	var movie Movie
-	//Encode the request from Json and Decode it into json movie variable
+	// Encode the request from JSON and decode it into JSON movie variable
 	_ = json.NewDecoder(r.Body).Decode(&movie)
-
-	// Make the movie ID with random number and store into Database(in this case Slices)
+	// Make the movie ID with a random number and store it into Database (in this case slices)
 	movie.ID = strconv.Itoa(rand.Intn(1000000))
 	movies = append(movies, movie)
-	//send the data in JSON format to the user
+	// Send the data in JSON format to the user
 	json.NewEncoder(w).Encode(movie)
 }
 
+// updateMovie updates a movie with the specified ID
 func updateMovie(w http.ResponseWriter, r *http.Request) {
 	// Set JSON content type
 	w.Header().Set("Content-Type", "application/json")
